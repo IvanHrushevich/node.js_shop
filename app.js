957f877737881a85c0421131552392bf3f4d9e7f
Ïvan Hrushevich <ivan.grushevich1@gmail.com>
2021-06-20 20:39:06 +0300

    implement User model, setup one-to-many relationship with products, add middleware adding user to req

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 const sequelize = require('./utils/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -17,14 +19,36 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(error => console.log(`error`, error));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize
     .sync()
     .then(res => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({ name: 'Ivan', email: 'test@test.com' });
+        }
+
+        return user;
+    })
+    .then(user => {
         app.listen(3000);
     })
     .catch(error => console.log(`error`, error));
